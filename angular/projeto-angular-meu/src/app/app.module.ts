9827c0e5c5fbb37accc5b69dcd8bcd07c3a23d0b
joseluizs <joseluiz.smarques@gmail.com>
2024-03-15 08:19:36 -0300

    Subindo exercício proposto

--- angular/projeto-angular-meu/src/app/app.module.ts
+++ angular/projeto-angular-meu/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/**Angular Material  */
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { Componente2Component } from './componente2/componente2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
